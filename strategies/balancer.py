60217f269dcc88b92b6e43a5deb16a66780f3d65
mazetone <108943296+mazetone@users.noreply.github.com>
2024-10-03 13:35:37 -0700

    wip: weights using pandas

--- strategies/balancer.py
+++ strategies/balancer.py
@@ -1,23 +1,21 @@
 import numpy as np
+import pandas as pd
 from scipy.optimize import minimize, Bounds
 
 class Balancer:
     def __init__(self):
         pass
 
-    def minimize_sharpe(self, data_matrix):
-        # data_matrix must have portfolio in column 1, and assets in 2-4
+    def minimize_sharpe(self, date, portfolio, data):
+        # data must have dates in first column
         # TODO: implement for pandas dataframes
-        # Define the objective sharpe to minimize
 
-        p = data_matrix[:,1]
-        x0 = np.array([(xi / np.sum(p)) for xi in p[i]]
-                     for i in range(0,p.shape[0]))
-        Q = np.cov([data_matrix[:,2], data_matrix[:,3], data_matrix[:,4]])
-        m = np.array([np.average(data_matrix[i,j] - data_matrix[i-1]
-                                 for i in range(1,data_matrix.shape[0]))
-                      for j in [2,3,4]])
-        
+        p = portfolio
+        x0 = np.array([(xi / np.sum(p)) for xi in p])
+        data_matrix = data.loc[:date].to_numpy()
+        Q = np.cov(data_matrix)
+        m = np.array([np.mean(np.diff(data_matrix,axis=0), axis=0)])
+
         # TODO: change fixed r_f to variable over period
 
         m_car = m - np.array([.02, .02, .02])
